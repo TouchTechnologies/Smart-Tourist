32b118bc3490dca2c202ed07736300e4712fa407
batmanpon <weerapon.3at@gmail.com>
2016-10-05 16:44:20 +0700

    เอพีไอ แฮกๆ

---
+++ SmartTourist/facebookAPI.swift
@@ -0,0 +1,9 @@
+//
+//  facebookAPI.swift
+//  SmartTourist
+//
+//  Created by weerapons suwanchatree on 10/5/2559 BE.
+//  Copyright © 2559 Thirawat Phannet. All rights reserved.
+//
+
+import Foundation
